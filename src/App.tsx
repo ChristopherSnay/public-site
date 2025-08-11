68af1b4e1f72bd2385393eb24e1d8362dde3652b
Christopher Snay <christopher.snay@gmail.com>
2025-08-11 18:52:05 -0400

    Cleanup

--- src/App.tsx
+++ src/App.tsx
@@ -8,55 +8,80 @@ function App() {
 
   return (
     <>
-      <AppBar position="static" color="transparent" elevation={0}>
+      <AppBar
+        elevation={0}
+        position="static"
+        color="transparent"
+      >
         <Toolbar>
-          <Link variant="h6" component={RouterLink}
-            to="/" underline="none" color={location.pathname === '/' ? 'primary' : 'textDisabled'} className="me-auto">
+          <Link
+            component={RouterLink}
+            variant="h6"
+            to="/"
+            underline="none"
+            color={location.pathname === '/' ? 'primary' : 'textDisabled'}
+            className="me-auto">
             christophersnay.com
           </Link>
           <Box sx={{ display: { xs: 'none', sm: 'flex' }, gap: 1 }}>
-            <Button variant="text" color={location.pathname === '/experience' ? 'warning' : 'primary'}
+            <Button
+              variant="text"
+              color={location.pathname === '/experience' ? 'warning' : 'primary'}
               onClick={() => navigate('/experience')}>
               Experience
             </Button>
-            <Button variant="text" color={location.pathname === '/education' ? 'warning' : 'primary'}
+            <Button
+              variant="text"
+              color={location.pathname === '/education' ? 'warning' : 'primary'}
               onClick={() => navigate('/education')}>
               Education
             </Button>
-            <Button variant="outlined" onClick={() => navigate('/contact')} color={location.pathname === '/contact' ? 'warning' : 'primary'}>
+            <Button
+              variant="outlined"
+              color={location.pathname === '/contact' ? 'warning' : 'primary'}
+              onClick={() => navigate('/contact')}>
               Contact
             </Button>
           </Box>
           <Box sx={{ display: { xs: 'flex', sm: 'none' }, gap: 1 }}>
             <Tooltip title="Experience">
-              <IconButton color={location.pathname === '/experience' ? 'warning' : 'primary'}
+              <IconButton
+                color={location.pathname === '/experience' ? 'warning' : 'primary'}
                 onClick={() => navigate('/experience')} >
                 <WorkOutline />
               </IconButton>
             </Tooltip>
             <Tooltip title="Education">
-              <IconButton color={location.pathname === '/education' ? 'warning' : 'primary'}
+              <IconButton
+                color={location.pathname === '/education' ? 'warning' : 'primary'}
                 onClick={() => navigate('/education')} >
                 <SchoolOutlined />
               </IconButton>
             </Tooltip>
             <Tooltip title="Contact">
-              <IconButton onClick={() => navigate('/contact')} color={location.pathname === '/contact' ? 'warning' : 'primary'} >
+              <IconButton
+                color={location.pathname === '/contact' ? 'warning' : 'primary'}
+                onClick={() => navigate('/contact')}>
                 <ContactMailOutlined />
               </IconButton>
             </Tooltip>
           </Box>
         </Toolbar>
-
       </AppBar>
 
       <main className="d-flex flex-column w-100">
         <Outlet />
       </main>
 
-      <Box component="footer" className="d-flex w-100 mt-auto p-5">
-        <Container maxWidth="sm" className="text-center">
-          <Typography color="textSecondary" variant="body2">
+      <Box
+        component="footer"
+        className="d-flex w-100 mt-auto p-5">
+        <Container
+          maxWidth="sm"
+          className="text-center">
+          <Typography
+            color="textSecondary"
+            variant="body2">
             {`Copyright © Christopher Snay ${new Date().getFullYear()}`}
           </Typography>
         </Container>
